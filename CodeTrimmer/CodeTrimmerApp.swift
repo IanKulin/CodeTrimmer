f0fc616bc1d334f7d5268f5231630940cd14d57b
Ian Bailey <IanKulin@kulin.com.au>
2022-10-03 11:36:50 +0800

    Initial

---
+++ CodeTrimmer/CodeTrimmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTrimmerApp.swift
+//  CodeTrimmer
+//
+//  Created by Ian Bailey on 3/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeTrimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeTrimmer/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  CodeTrimmer
+//
+//  Created by Ian Bailey on 3/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var codeSnippet = ""
+    @State private var fontSize = 12.0
+    
+    // TODO: initialise window at a reasonable size
+    // TODO: add "copy all" button
+    // TODO: icons
+    // TODO: Help Text
+    // TODO: Web page (Github?)
+    
+    var displayFont: Font {
+        Font.system(size: CGFloat(fontSize),
+                    design: .monospaced)
+    }
+    
+    var body: some View {
+        Section {
+            TextEditor(text: $codeSnippet)
+                .foregroundColor(.primary)
+                .padding()
+                .font(displayFont)
+                .toolbar {
+                    ToolbarItemGroup {
+                        Slider(
+                            value: $fontSize,
+                            in: 8...120,
+                            minimumValueLabel:
+                                Text("A").font(.system(size: 8)),
+                            maximumValueLabel:
+                                Text("A").font(.system(size: 16))
+                        ) {
+                            Text("Font Size (\(Int(fontSize)))")
+                        }
+                        .frame(width: 150)
+                        Divider()
+                        Button("Strip Spaces")
+                            { codeSnippet = stripSpaces(codeSnippet)}
+                            .background(.white)
+                            .foregroundColor(.primary)
+                            .cornerRadius(5)
+                            .shadow(radius: 2)
+                    }
+                }
+                .navigationTitle("CodeTrimmer")
+        }
+    }
+}
+
+
+fileprivate func stripSpaces(_ codeLines: String ) -> String {
+    // break into lines
+    var lines = codeLines.components(separatedBy: "\n")
+    var minCount = Int.max
+    // step though the lines and count how many spaces, save the minimum amount
+    for line in lines {
+        let leadingSpaces = line.prefix(while: { $0 == " " }).count
+        minCount = leadingSpaces < minCount ? leadingSpaces : minCount
+    }
+    // step though the lines again, and trim the min amount from each line
+    for i in 0..<lines.count {
+        lines[i] = String(lines[i].dropFirst(minCount))
+    }
+    // stitch it back up
+    return lines.joined(separator: "\n")
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
